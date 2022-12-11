1710928ea66234a3723847f9a7f5a0df2355e742
jimcode123 <jimcode44@gmail.com>
2022-12-11 16:58:40 +0800

    First commit

---
+++ py/foo.py
@@ -0,0 +1,16 @@
+import ZeekControl.plugin
+from ZeekControl import config
+
+class Foo(ZeekControl.plugin.Plugin):
+    def __init__(self):
+        super(Foo, self).__init__(apiversion=1)
+
+    def name(self):
+        return "foo"
+
+    def pluginVersion(self):
+        return 1
+
+    def init(self):
+        self.message("The newfoo plugin is initialized")
+        return True
